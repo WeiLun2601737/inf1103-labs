3d8886542171bd81e7b4aed353dc60b888aeb406
CHEE WEI LUN <2601737@sit.singaporetech.edu.sg>
2026-09-04 11:19:51 +0800

    Assignment Variables

--- hello.py
+++ hello.py
@@ -7,15 +7,28 @@
 
 #Activity 2:
 
-print("=============")
-print("Welcome here")
-print("My first post!")
-print("=============")
+# print("=============")
+# print("Welcome here")
+# print("My first post!")
+# print("=============")
 
-username = "cool_creator"
-bio= "Fun Blogger"
+# username = "cool_creator"
+# bio= "Fun Blogger"
+# followers = 100
+
+# print("Username:", username)
+# print("Bio:", bio)
+# print("Followers:", followers)
+
+#Activity 3:
+
 followers = 100
 
-print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+followers += 50  # Increase followers by 50
+print("Day 1:", followers)
+
+followers += 20  # Increase followers by 20
+print("Day 2:", followers)
+
+followers -= 10  # Decrease followers by 10
+print("Day 3:", followers)
