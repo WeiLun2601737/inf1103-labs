90242f082a5bedf873fe5ca4b1a3cc1d8e0f792b
CHEE WEI LUN <2601737@sit.singaporetech.edu.sg>
2026-09-04 11:17:49 +0800

    Added Profile Variables

--- hello.py
+++ hello.py
@@ -1,4 +1,21 @@
-print("============")
-print("Welcome Here")
+# Activity 1:
+
+# print("=============")
+# print("Welcome here")
+# print("My first post!")
+# print("=============")
+
+#Activity 2:
+
+print("=============")
+print("Welcome here")
 print("My first post!")
-print("============")
+print("=============")
+
+username = "cool_creator"
+bio= "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
