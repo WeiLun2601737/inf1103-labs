374d72279d0ab81c7fac498b3eefa6c82794ef88
CHEE WEI LUN <2601737@sit.singaporetech.edu.sg>
2026-09-18 02:09:56 +0800

    Requirement Function

--- LAB3/modular_auditor.py
+++ LAB3/modular_auditor.py
@@ -1,25 +1,44 @@
+def get_valid_input():
+    stock = input ("Enter stock quantity (Type 'Quit' to exit): ")
+
+    if stock.lower() == "quit":
+            return "quit"
+    
+    if not stock.isdigit():
+            print ("Error: Enter a valid number.")
+            return None
+
+    return int(stock)
+
+def process_delivery(current_total,new_value):
+      current_total += new_value
+      return current_total
+
+def calculate_tax(stock):
+    tax = stock * 0.10
+    return tax
+
+def generate_report(total_units,failed_attempts):
+    print("\nTotal Units:", total_units)
+    print("Number of Failed/Rejected Entries:", failed_attempts)
+
 inventory = 0
 failedentry = 0
 delivery = 0
-totaltax = 0.0
-
+totaltax = 0
+    
 while True:
-    stock = input ("Enter stock quantity (Type 'Quit' to exit): ")
-    if stock.lower() == "quit":
-        break
-
-    if not stock.isdigit():
-        print ("Error: Enter a valid number.")
+    stock = get_valid_input()
+    if stock == "quit":
+          break
+    if stock is None:
         failedentry += 1
         continue
 
-    inventory += int(stock)
-    tax = int(stock) * 0.10
-    totaltax = tax + totaltax
+    inventory = process_delivery(inventory, int(stock))
+    totaltax += calculate_tax(int(stock))
     delivery += 1
 
-
-print("\nTotal Unit:", inventory)
-print("Number of Failed/Rejected Entries:", failedentry)
-print ("Total Tax:", f"{totaltax:.2f}")
+generate_report(inventory, failedentry)
+print ("Total Tax: $", f"{totaltax:.2f}")
 print("Number of Successful Deliveries:", delivery)
